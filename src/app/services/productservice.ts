65e7cb74996e877ae88eaa1f3013b63f3458e41c
Hamza hanfi <hanfihamza20@gmail.com>
2024-02-20 04:27:34 +0100

    Setting Up ProductService With Tests

---
+++ src/app/services/productservice.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  baseUrl: string = 'http://localhost:8080';
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/products`);
+  }
+}
